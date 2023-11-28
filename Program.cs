6adc08ddf8bfb57cbcd6f2b0b56c25a7e28a3c2d
Egor Korol <79230634+wntmorder@users.noreply.github.com>
2023-11-28 13:56:42 +0300

    Update the netcoreapp version, add the appsettings.json configuration file

--- Program.cs
+++ Program.cs
@@ -9,19 +9,41 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace VoiceRecognationBot
 {
     class Program
     {
-        private static ITelegramBotClient botClient = new TelegramBotClient("");    // Your Telegram API Token 
-        private static string apiToken = "";                                        // Your Assembly-AI API Token 
-        private static string filePath = @"";                                       // Path to the audio file
+        private static IConfiguration configuration;
+        private static ITelegramBotClient botClient;
+        private static string apiAssemblyAI;
+        private static string filePath;
 
         public static async Task Main(string[] args)
         {
+            configuration = BuildConfiguration();
+
+            Initialize();
+
             botClient.StartReceiving(Update, Error);
-            Thread.Sleep(int.MaxValue);
+            Console.WriteLine("Bot started.");
+            await Task.Delay(-1);
+        }
+        private static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .Add(new JsonConfigurationSource { Path = "appsettings.json", Optional = false, ReloadOnChange = true })
+                .Build();
+        }
+        private static void Initialize()
+        {
+            string apiTelegram = configuration["TelegramApiKey"];
+            apiAssemblyAI = configuration["AssemblyAIApiKey"];
+            filePath = configuration["VoiceMessageFilePath"];
+
+            botClient = new TelegramBotClient(apiTelegram);
         }
         private static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
@@ -36,34 +58,33 @@ namespace VoiceRecognationBot
                 {
                     await DownloadVoiceMessage(update.Message.Voice.FileId);
 
-                    var uploadedFileUrl = await UploadFileAsync(apiToken, filePath);
+                    string uploadedFileUrl = await UploadFileAsync(apiAssemblyAI, filePath);
                     if (uploadedFileUrl == null)
                     {
                         Console.WriteLine("Failed to upload file.");
                         return;
                     }
-                    var message = await botClient.SendTextMessageAsync(update.Message.Chat, "Transcription in progress...");
-                    var messageId = message.MessageId;
+                    Message message = await botClient.SendTextMessageAsync(update.Message.Chat, "Transcription in progress...", cancellationToken: token);
+                    int messageId = message.MessageId;
 
-                    var transcript = await GetTranscriptAsync(apiToken, uploadedFileUrl);
+                    string transcript = await GetTranscriptAsync(apiAssemblyAI, uploadedFileUrl);
 
-                    await botClient.DeleteMessageAsync(update.Message.Chat, messageId);
+                    await botClient.DeleteMessageAsync(update.Message.Chat, messageId, cancellationToken: token);
                     await SendMessage(update.Message.Chat, transcript);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Error: " + e.Message);
+                    await SendMessage(update.Message.Chat, "Error on the server side.");
                 }
             }
         }
         private static async Task DownloadVoiceMessage(string fileId)
         {
-            var voiceMessage = await botClient.GetFileAsync(fileId);
+            Telegram.Bot.Types.File voiceMessage = await botClient.GetFileAsync(fileId);
 
-            using (var fileStream = new FileStream(filePath, FileMode.Create))
-            {
-                await botClient.DownloadFileAsync(voiceMessage.FilePath, fileStream);
-            }
+            using FileStream fileStream = new(filePath, FileMode.Create);
+            await botClient.DownloadFileAsync(voiceMessage.FilePath, fileStream);
         }
         private static async Task SendMessage(Chat chatId, string message)
         {
@@ -72,12 +93,12 @@ namespace VoiceRecognationBot
               text: message
             );
         }
-        private static async Task<string> UploadFileAsync(string apiToken, string path)
+        private static async Task<string> UploadFileAsync(string apiAssemblyAI, string path)
         {
-            using var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(apiToken);
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(apiAssemblyAI);
 
-            using var fileContent = new ByteArrayContent(System.IO.File.ReadAllBytes(path));
+            using ByteArrayContent fileContent = new(System.IO.File.ReadAllBytes(path));
             fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
 
             HttpResponseMessage response;
@@ -94,7 +115,7 @@ namespace VoiceRecognationBot
             if (response.IsSuccessStatusCode)
             {
                 string responseBody = await response.Content.ReadAsStringAsync();
-                var json = JObject.Parse(responseBody);
+                JObject json = JObject.Parse(responseBody);
                 return json["upload_url"].ToString();
             }
             else
@@ -103,42 +124,40 @@ namespace VoiceRecognationBot
                 return null;
             }
         }
-        public static async Task<string> GetTranscriptAsync(string apiToken, string audioUrl)
+        public static async Task<string> GetTranscriptAsync(string apiAssemblyAI, string audioUrl)
         {
-            var data = new Dictionary<string, string>()
+            Dictionary<string, string> data = new()
             {
                 { "audio_url", audioUrl }
             };
 
-            using (var client = new HttpClient())
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Add("authorization", apiAssemblyAI);
+            StringContent content = new(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("https://api.assemblyai.com/v2/transcript", content);
+            string responseContent = await response.Content.ReadAsStringAsync();
+            dynamic responseJson = JsonConvert.DeserializeObject<dynamic>(responseContent);
+
+            string transcriptId = responseJson.id;
+            string pollingEndpoint = $"https://api.assemblyai.com/v2/transcript/{transcriptId}";
+
+            while (true)
             {
-                client.DefaultRequestHeaders.Add("authorization", apiToken);
-                var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
-                HttpResponseMessage response = await client.PostAsync("https://api.assemblyai.com/v2/transcript", content);
-                var responseContent = await response.Content.ReadAsStringAsync();
-                var responseJson = JsonConvert.DeserializeObject<dynamic>(responseContent);
+                HttpResponseMessage pollingResponse = await client.GetAsync(pollingEndpoint);
+                string pollingResponseContent = await pollingResponse.Content.ReadAsStringAsync();
+                dynamic pollingResponseJson = JsonConvert.DeserializeObject<dynamic>(pollingResponseContent);
 
-                string transcriptId = responseJson.id;
-                string pollingEndpoint = $"https://api.assemblyai.com/v2/transcript/{transcriptId}";
-
-                while (true)
+                if (pollingResponseJson.status == "completed")
                 {
-                    var pollingResponse = await client.GetAsync(pollingEndpoint);
-                    var pollingResponseContent = await pollingResponse.Content.ReadAsStringAsync();
-                    var pollingResponseJson = JsonConvert.DeserializeObject<dynamic>(pollingResponseContent);
-
-                    if (pollingResponseJson.status == "completed")
-                    {
-                        return pollingResponseJson.text;
-                    }
-                    else if (pollingResponseJson.status == "error")
-                    {
-                        throw new Exception($"Transcription failed: {pollingResponseJson.error}");
-                    }
-                    else
-                    {
-                        Thread.Sleep(1000);
-                    }
+                    return pollingResponseJson.text;
+                }
+                else if (pollingResponseJson.status == "error")
+                {
+                    throw new Exception($"Transcription failed: {pollingResponseJson.error}");
+                }
+                else
+                {
+                    Thread.Sleep(1000);
                 }
             }
         }
